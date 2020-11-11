bb14a43ff1c58f22bfc06e8ea84fabf64251ca60
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2020-11-11 12:41:40 -0700

    complete second project

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function changeSize(){
+    var newText = "Hello... ";                                      // Create new text variable
+    newText+= "world!";                                             // Concatenate it
+    document.getElementById("text-change").innerHTML = newText;    // Change the innerHTML
+};                         
