ecc4e0963d19c05f3c8a5966fc134b7839d01ac1
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2020-11-11 12:26:49 -0700

    add comments and new expressions for clicking

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,3 +1,11 @@
-var A = "\"This is a string\"", B = " and this is a different string";  // Makes the two strings
-var A = A.fontcolor("Blue"), B = B.fontcolor("red")                     // Changes the color of the two strings
-document.write(A + B);                                                  // Concatenates and prints to page
+function changeColor(){                                                                         //
+    var newColors = document.getElementById("change-color").innerHTML;                          // Grabs the innerHTML to be used later   
+    var newColors = newColors.fontcolor("green");                                               // Changes only the color for the HTML
+    document.getElementById("change-color").innerHTML= newColors;                               // Sets the new color with new values
+};                                                                                              //
+
+function clickAlert(){                                                                          //
+    var A = "This is the text that appears when you click the", B = " button too!";             // The two strings we will concatenate
+    window.alert("This is a warning message for pressing the button")                           // Puts alert on screen when called
+    document.write(A + B)                                                                       // Concatenates the two strings we wrote
+};                                                                                              //
