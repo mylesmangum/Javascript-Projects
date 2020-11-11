b1ce73d8ca415a898129f34f35f5720e90e18e59
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2020-11-11 12:12:32 -0700

    make first project for js

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,3 @@
+var A = "\"This is a string\"", B = " and this is a different string";  // Makes the two strings
+var A = A.fontcolor("Blue"), B = B.fontcolor("red")                     // Changes the color of the two strings
+document.write(A + B);                                                  // Concatenates and prints to page
