329ee89705cf439c67e2e93410de9a6dd000c58f
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2021-04-23 09:42:37 -0600

    nothing just bug testing

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,11 +1,11 @@
 
 function getRooms(){
     var apartment = {                                   // Initializing the KVP's
-        rooms:2,
-        temperature:72,
+        rooms: 2,
+        temperature: 72,
         address:"970 S, Univeristy Street",
         building:"B"
     };
     delete apartment.rooms;                       // Deleting the rooms
-    document.getElementById("dictionary").innerHTML = apartment.rooms;  // Get the value for the key in rooms
+    document.getElementById("dictionary").innerHTML = String(apartment.rooms);  // Get the value for the key in rooms
 };
