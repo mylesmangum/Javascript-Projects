79dfa8b02904925c584b91e3aff6f1fd75f59644
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2021-04-23 11:30:52 -0600

    fix broken code from wrong wording and syntax

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,40 +1,42 @@
 function doRandom(){                                    // Each function is called by a different button in the HTML                            
     var randomNum = Math.random() * 100;                // Multiplies and gets a random number from 0 to 100
     window.alert(randomNum);                            // Alerts
-};
+}
       
 function doAddition(){
     var addNum = 2 + 2;    //Adds
     window.alert(addNum);  //Alerts
-};
+}
 
 function doSubtraction(){
     var subNum = 12 - 30;   // Subtracts
     window.alert(subNum);   // Prints num out in alert box
-};
+}
 
 function doMultiplication(){
     var multNum = 20 * 100; // Multiplies
     window.alert(multNum);  // Prints num in alert box
-};
+}
 
 function doDivision(){
     var divNum = 21 / 2;    // Divides
     window.alert(divNum);   // Prints it out
-};
+}
 
 function doModulation(){
     var modNum = 10 % 3;    // Finds mod of 10
-    alert.write(modNum);    // Prints out said mod
+    window.alert(modNum);    // Prints out said mod
 }
 
 
 function plusOne(){
-    var oneMore = 10++;     // Adds one to 10
-    alert.write(oneMore);   // Prints it out
+    var oneMore = 10;     // Adds one to 10
+    oneMore++;
+    window.alert(oneMore);   // Prints it out
 }
 
 function minusOne(){
-    var oneLess = 10--;     // Subtracts one from 10
-    alert.write(oneLess);   // Prints
+    var oneLess = 10;       // Subtracts one from 10
+    oneLess--;
+    window.alert(oneLess);   // Prints
 }
