82a6869823128edb3278876992e8487dfcfe0116
mylesmangum <70152377+mylesmangum@users.noreply.github.com>
2021-04-27 10:38:57 -0600

    comment and fix broken code

--- Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
+++ Basic JavaScript Projects/Project10_loops_arrays/JS/main.js
@@ -1,25 +1,31 @@
 
 function runWhile(){
-    document.getElementById("looper").style.display = "inline"
+    //Reveals the hidden element
+    document.getElementById("looper").style.display = "inline"                      
     var i = 0;
+    // Weird loop thing I made that goes forever except it has a break  
     while(1 == 1){
         i++;
+        //Will change the value to whatever i is, but because there is no pause it goes to 31
         document.getElementById("looper").innerText = "This will count to: " + i;
         if(i > 30){
             break;
         } 
     }
 }
-
+//Basic for loop function that writes to 24 each on a new line
 function runFor(){
     for(var i = 0; i < 25; i++){
-        document.write(i);
+        document.write(i + "<br>");
     }
 }
 
 function showArray(){
-    let animals = ['Dog', 'Cat', 'Horse', 'Lizard', 'Dog'];
+    // Set values
+    let animals = ['Dog', 'Cat', 'Horse', 'Lizard', 'Dog'];  
+    // Loop through the whole list
     for(var i = 0; i < animals.length; i++){
+        // Print it out with a new line
         document.write(animals[i] + "<br>");
     }
 }
